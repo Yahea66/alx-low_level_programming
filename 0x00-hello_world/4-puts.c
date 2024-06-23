5b2a137a8b5c0c0c840958b6ffe5193af3153fec
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-06-23 15:47:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
-main - prints: Programming is like building a multilingual puzzle
-*/
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    puts("\"programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
