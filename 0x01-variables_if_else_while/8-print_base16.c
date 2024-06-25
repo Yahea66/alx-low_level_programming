380080c2163eae0f61a7e82a13a6b060c83d73ec
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-06-24 18:22:48 -0700

    not sure

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all the numbers of base 16 in lowercase.
+ *
+ *  return: always 0 (success)
+ */
+
+int main(void)
+{
+int d;
+char alph;
+for (d = '0'; d <= 9; d++)
+{
+putchar(d);
+}
+for (alph = 'a'; alph <= 'f'; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
