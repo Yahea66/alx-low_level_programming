1ec95450b5e96626e6d56145adfc5561c39bc787
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-06-26 06:47:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main -   prints _putchar, followed by a new line.
-*
-* return: always 0 (success) 
+* prints _putchar
+* return: always  0 (success)
 */
 int main(void) 
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
