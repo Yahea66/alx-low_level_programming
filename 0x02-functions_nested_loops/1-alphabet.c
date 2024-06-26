55b34265c2c7b21a23f103cc0ddb3a821342859d
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-06-26 07:42:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void 
+ */ 
+
+void print_alphabet(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+_putchar(alph);
+}
+_putchar('\n');
+}
