7ad92641344dda936ead7e9da651f4740cb083bb
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-06-28 19:05:05 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - prints a triangle. 
+ *
+ * Return: Void
+ */
+void print_triangle(int size)
+{
+int i, j, k;
+if (size > 0)
+{
+for (i = 0; i <= size; i++)
+{
+for (j = size; j > i; j--)
+{
+_putchar(32);
+}
+for (k = 0; k < i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+} 
+} 
