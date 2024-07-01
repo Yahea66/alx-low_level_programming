684a99db31658b4b181979ddb61ff378bbf8225d
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-01 08:42:37 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: pointer to an integer
+ * @b: pointer to an integer
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+int i =0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
