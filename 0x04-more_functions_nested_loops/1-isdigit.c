02280b48cd1d35384a031f11acfa68cf9f0723cd
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-05 09:02:24 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,22 @@
 /**
- * main - checks for a digit (0 through 9). 
+ * _isdigit - Checks for a digit (0 through 9).
+ * @c: The character to be checked
  *
- * Return: Returns 1 if c is a digit
+ * Description: This function checks if the given character is a digit
+ * (0 through 9). If the character is a digit, it returns 1.
+ * Otherwise, it returns 0.
+ *
+ * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c) 
+int _isdigit(int c)
 {
-char i = '0';
-int v = 0;
-for (; i <= '9'; i++)
+char i;
+for (i = '0'; i <= '9'; i++)
 {
 if (c == i)
 {
-v = 1;
-break;
+return (1);
 }
 }
-return (v);
+return (0);
 }
