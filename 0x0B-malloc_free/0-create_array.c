6b1209ab986614eb83b2ca4621e8c18675dda619
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-16 06:13:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,21 +4,30 @@
 /**
  * create_array - creates an array of chars
  * @size: number of chars in the array
- * @c: the char to Initialize the array 
+ * @c: the char to initialize the array
  *
- * Return: NULL or a pointer to an array 
+ * Return: NULL or a pointer to the array
  */
 char *create_array(unsigned int size, char c)
 {
-char *arr = malloc(sizeof(unsigned int) * size); 
-unsigned int i;
-if (size == 0)
-{
-return (NULL);
-} 
-for (i = 0; i < size; i++) 
-{
-arr[i] = c;
-} 
-return (arr);
-} 
+    char *arr;
+    unsigned int i;
+
+    if (size == 0)
+    {
+        return NULL;
+    }
+
+    arr = malloc(sizeof(char) * size);
+    if (arr == NULL)
+    {
+        return NULL;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        arr[i] = c;
+    }
+
+    return arr;
+}
