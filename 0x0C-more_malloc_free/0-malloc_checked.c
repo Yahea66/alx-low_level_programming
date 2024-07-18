0b1a8b17ef26b9e05d785bc6c60e7132599ce588
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-18 07:25:43 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*malloc_checked - allocates memory using malloc.
+*@b: number of bytes 
+*
+*Return: Void
+*/
+void *malloc_checked(unsigned int b)
+{
+void *arr = malloc(b);
+if (arr == NULL)
+{
+exit(98);
+} 
+return (arr);
+} 
