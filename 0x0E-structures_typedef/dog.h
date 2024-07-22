ba9f35ec210c148e6923e496c455b91d25455b71
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-22 16:18:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dog info.
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: construct a dog description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
