804567498a25736a13243bb8ff6f2f1795e22d49
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-22 17:10:33 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,18 @@
 /**
-*init_dog - initialize a variable of type struct dog
-*@d: the struct variable 
-*@name: Name of the dog 
-*@age: Age of the dog
-*@owner: Owner of the dog 
-*
-*Return: Void
-*/
+ * init_dog - initialize a variable of type struct dog
+ * @d: the struct variable to be initialized
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d =! NULL) 
+if (d != NULL)
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
-} 
-} 
+}
+}
