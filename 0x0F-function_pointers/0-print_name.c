0e5c9b29346277a53550f669189e2ff4ffc16fac
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-07-24 15:55:17 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,15 @@
 /**
  * print_name - prints a name.
  * @name: pointer to a string to be printed
- * @f: pointer to a function 
+ * @f: pointer to a function
  *
- * Return: Void 
+ * Return: Void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL) 
+if (name == NULL || f == NULL)
 {
 return;
-} 
+}
 f(name);
-} 
+}
